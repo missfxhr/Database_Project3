e730c34649c58390118e0f01e1cd63f94e37909d
huanshi.hy <huanshi.hy@alibaba-inc.com>
2015-11-15 17:59:31 -0600

    add curcourses

--- src/main/java/ClientMain.java
+++ src/main/java/ClientMain.java
@@ -1,6 +1,20 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class ClientMain {
     public static void main(String[] argvs){
         WorkFlow workFlow = new WorkFlow();
-        if (!workFlow.login()) return;
+        try {
+            if (!workFlow.login()) {
+                return;
+            }
+            workFlow.listCurrentCourses();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }
     }
 }
--- src/main/java/WorkFlow.java
+++ src/main/java/WorkFlow.java
@@ -1,7 +1,11 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 
 public class WorkFlow {
+    private Student currentStudent;
+    private Connection conn;
 
     public Student getStudent(){
         return currentStudent;
@@ -17,62 +21,81 @@ public class WorkFlow {
         }
     }
 
-    boolean login(){
+    boolean login() throws IOException, SQLException{
+        //initialization
         serverConnInit();
-
         String username;
         String password;
         CallableStatement cStmt = null;
         ResultSet rs = null;
 
-        try{
-            System.out.println("Enter Student Name:");
-            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-            username = bufferRead.readLine();
-            System.out.println("Enter Student Password:");
-            bufferRead = new BufferedReader(new InputStreamReader(System.in));
-            password = bufferRead.readLine();
+        // get username and password
+        System.out.println("Enter Student Name:");
+        BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+        username = bufferRead.readLine();
+        System.out.println("Enter Student Password:");
+        bufferRead = new BufferedReader(new InputStreamReader(System.in));
+        password = bufferRead.readLine();
 
-            String callProcedure = "{call login(?, ?)}";
+        // call procedure
+        String callProcedure = "{call login(?, ?)}";
+        cStmt = conn.prepareCall(callProcedure);
+        cStmt.setString(1, username);
+        cStmt.setString(2, password);
 
-            cStmt = conn.prepareCall(callProcedure);
-            cStmt.setString(1, username);
-            cStmt.setString(2, password);
-            if (!cStmt.execute()) {
-                System.out.println("Login Failed - Wrong Student Name or Wrong Password");
-                return false;
-            }
-            rs = cStmt.getResultSet();
-            while(rs.next()) {
-                currentStudent = new Student(rs.getInt("id"), rs.getString("name"), rs.getString("password"));
-            }
-            System.out.println(currentStudent.getName() + ", Welcome!");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch(SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-        } finally{
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException sqlEx) {
-                }
-            }
-            if (cStmt != null) {
-                try {
-                    cStmt.close();
-                } catch (SQLException sqlEx) {
-                }
-            }
+        // check validity
+        if (!cStmt.execute()) {
+            System.out.println("Login Failed - Wrong Student Name or Wrong Password");
+            return false;
         }
+        rs = cStmt.getResultSet();
+        while(rs.next()) {
+            currentStudent = new Student(rs.getInt("id"), rs.getString("name"), rs.getString("password"));
+        }
+        System.out.println(currentStudent.getName() + ", Welcome!");
+
+        releaseConnection(cStmt, rs);
         return true;
     }
-    void listCurrentCourses(){}
+
+    void listCurrentCourses() throws SQLException {
+        //initialization
+        CallableStatement curCourses = null;
+        ResultSet rowCurCourses = null;
+
+        // call procedure
+        String callProcedure = "{call list_current_courses(?)}";
+        curCourses = conn.prepareCall(callProcedure);
+        curCourses.setInt(1, currentStudent.getStudentId());
+
+        // list courses
+        if (!curCourses.execute()) {
+            System.out.println("No current courses!");
+            return;
+        }
+
+        rowCurCourses = curCourses.getResultSet();
+        System.out.println("Current Courses:");
+        int colNums = rowCurCourses.getMetaData().getColumnCount();
+        while(rowCurCourses.next()) {
+            for (int col = 1; col <= colNums; col++) {
+                Object colVal = rowCurCourses.getObject(col) != null? rowCurCourses.getObject(col): "n.a.";
+                System.out.print(colVal + " ");
+            }
+            System.out.println();
+        }
+
+        releaseConnection(curCourses, rowCurCourses);
+    }
     void listTranscript(){}
     void listCourseDetail(){}
-    private Student currentStudent;
-    private Connection conn;
+
+    void releaseConnection(CallableStatement cStmt, ResultSet rs) throws SQLException {
+        if (rs != null) {
+            rs.close();
+        }
+        if (cStmt != null) {
+            cStmt.close();
+        }
+    }
 }
